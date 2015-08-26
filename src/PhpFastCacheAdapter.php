82d97a2cf98c6deafde1a9d70d94bba84a4d4436
jeremykendall <jeremy@jeremykendall.net>
2015-08-26 09:10:22 -0500

    Add README and PhpFastCacheAdapter

---
+++ src/PhpFastCacheAdapter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace GraphStory\Cache;
+
+/**
+ * PhpFastCache Adapter.
+ */
+class PhpFastCacheAdapter
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function get($key)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function set($key, $value)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function delete($key)
+    {
+    }
+}
